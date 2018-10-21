f7e24a049c09ce5d13cdd63adb4fbc4525d9651d
semainda <semaindas@gmail.com>
2018-10-21 18:35:06 +0300

    ft #161262916 Add get roles endpoint

--- app/api/v1/views/users/roles.py
+++ app/api/v1/views/users/roles.py
@@ -30,6 +30,19 @@ class Roles(Resource, Initializer):
     """Class that creates and ruturn roles"""
     def __init__(self):
         super().__init__()
+    
+    @jwt_required
+    def get(self):
+        """Method that return products"""
+        if get_jwt_identity():
+            user_role_name = self.auth.return_role_name(get_jwt_identity())
+            if user_role_name == "store_owner":
+                self.response = self.role.get_roles()
+            else:
+                self.response = self.resp.forbidden_user_access_response()
+        else:
+            self.response = self.resp.unauthorized_user_access_responses()
+        return self.response
 
     @jwt_required
     def post(self):
